6c69bf00bf6d7f02cbec59c30733c6bfecd0d0b8
Yuuki Tsujimoto <tsujimon850734@gmail.com>
2020-03-31 18:19:07 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react';
+import { Button, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import createStyles from '@material-ui/core/styles/createStyles';
 
 function App() {
+  const [ tasks, setTasks ] = useState([]);
+  const [ form, setForm ] = useState('');
+  console.log(form);
+
+  console.log(tasks);
+  console.log(typeof(tasks));
+
+  const onAddClicked = () => {
+    console.log(tasks);
+    console.log(typeof(tasks));
+    setTasks([...tasks, form]);
+    setForm('');
+  }
+  const onDeleteClicked = (taskIndex) => {
+    setTasks(tasks.filter((task, index) => index !== taskIndex));
+  };
+
+  const onFormChange = useCallback((ev) => {
+    setForm(ev.target.value);
+  }, [form]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {tasks && tasks.map((task, index) => (
+        <div>
+          <Typography>{task}</Typography>
+          <Button onClick={() => onDeleteClicked(index)}>削除</Button>
+        </div>
+      ))}
+      <div>
+        <TextField
+          margin="normal"
+          variant={'outlined'}
+          value={form}
+          onChange={onFormChange}
+        />
+        <Button onClick={onAddClicked}>追加</Button>
+      </div>
     </div>
   );
 }
